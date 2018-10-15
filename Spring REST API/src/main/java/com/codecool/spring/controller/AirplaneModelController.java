60e8ac65f5bf423f83b2a1caeb815481b1de4eb7
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-15 20:51:18 +0200

    Create getAllAirplaneModels method

--- Spring REST API/src/main/java/com/codecool/spring/controller/AirplaneModelController.java
+++ Spring REST API/src/main/java/com/codecool/spring/controller/AirplaneModelController.java
@@ -1,4 +1,22 @@
 package com.codecool.spring.controller;
 
+import com.codecool.spring.model.AirplaneModel;
+import com.codecool.spring.service.AirplaneModelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class AirplaneModelController {
+
+    @Autowired
+    private AirplaneModelService airplaneModelService;
+
+    @GetMapping("/producers/{id}/airplanes")
+    public List<AirplaneModel> getAllAirplaneModels(@PathVariable long id) {
+        return airplaneModelService.getAllAirplaneModels(id);
+    }
 }
