36dff19de90c357ffb1da8f1beca26a8f729efc0
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-15 20:34:36 +0200

    Implement AirplaneModel class

--- Spring REST API/src/main/java/com/codecool/spring/model/AirplaneModel.java
+++ Spring REST API/src/main/java/com/codecool/spring/model/AirplaneModel.java
@@ -1,10 +1,57 @@
 package com.codecool.spring.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "airplane_models")
 public class AirplaneModel {
+
 	@Id
+	@GeneratedValue
+	@Column(name = "model_id")
 	private long id;
+	@Column(name = "model_name")
+	private String modelName;
+	@Column(name = "maxSeat")
+	private int maxSeat;
+
+	@ManyToOne
+	private Producer producer;
+
+	public AirplaneModel() {
+	}
+
+	public AirplaneModel(String modelName, int maxSeat) {
+		this.modelName = modelName;
+		this.maxSeat = maxSeat;
+		this.producer = new Producer();
+	}
+
+	public Producer getProducer() {
+		return producer;
+	}
+
+	public void setProducer(Producer producer) {
+		this.producer = producer;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public int getMaxSeat() {
+		return maxSeat;
+	}
+
+	public void setMaxSeat(int maxSeat) {
+		this.maxSeat = maxSeat;
+	}
+
+	public long getId() {
+		return id;
+	}
 }
