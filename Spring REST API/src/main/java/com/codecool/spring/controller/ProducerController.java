bfbd22cbf709a6e3557fb5dd74c19c08a055b6f3
elzacodecool <elliegolec@gmail.com>
2018-10-16 12:59:34 +0200

    Add update method to controller

--- Spring REST API/src/main/java/com/codecool/spring/controller/ProducerController.java
+++ Spring REST API/src/main/java/com/codecool/spring/controller/ProducerController.java
@@ -1,41 +1,5 @@
 package com.codecool.spring.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.codecool.spring.model.Producer;
-import com.codecool.spring.service.ProducerService;
-
-@RestController
 public class ProducerController {
-	
-	@Autowired
-	private ProducerService producerService;
-	
-	@GetMapping("/producers")
-	public List<Producer> getAllProducers() {
-		return producerService.getAllProducers();
-	}
-	
-	@GetMapping("/producers/{id}")
-	public Producer getProducer(@PathVariable long id) {
-		return producerService.getProducer(id);
-	}
 
-	@PostMapping("/producers")
-	public void addProducer(@RequestBody Producer producer) {
-		producerService.add(producer);
-	}
-	
-	@PutMapping("/producers/{id}")
-	public void updateProducer(@RequestBody Producer producer, @PathVariable long id) {
-		producerService.update(id, producer);
-	}
 }
