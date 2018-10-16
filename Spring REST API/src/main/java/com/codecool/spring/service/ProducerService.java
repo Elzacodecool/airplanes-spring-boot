ed81d13b5cf01adaeb2d900f321eb8b730762604
elzacodecool <elliegolec@gmail.com>
2018-10-16 13:00:14 +0200

    Add update producer to service

--- Spring REST API/src/main/java/com/codecool/spring/service/ProducerService.java
+++ Spring REST API/src/main/java/com/codecool/spring/service/ProducerService.java
@@ -1,5 +1,7 @@
 package com.codecool.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +18,23 @@ public class ProducerService {
 		this.producerRepository = pr;
 	}
 	
-	public void createProducent(String name, String owner) {
-		this.producerRepository.save(new Producer(name, owner));
+	public List<Producer> getAllProducers() {
+		return (List<Producer>) producerRepository.findAll();
 	}
+	
+	public Producer getProducer(long id) {
+		return producerRepository.findById(id).get();
+	}
+	
+	public void add(Producer producer) {
+		producerRepository.save(producer);
+	}
+
+	public void update(long id, Producer producerDetails) {
+		Producer currnetProducer = producerRepository.findById(id).get();
+		currnetProducer.setName(producerDetails.getName());
+		currnetProducer.setOwner(producerDetails.getOwner());
+		producerRepository.save(currnetProducer);
+	}
+	
 }
