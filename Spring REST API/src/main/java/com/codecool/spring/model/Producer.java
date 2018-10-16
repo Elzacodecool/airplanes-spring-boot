a3ae39b9d34e4752786877543a0c197cb94980ce
elzacodecool <elliegolec@gmail.com>
2018-10-16 13:58:08 +0200

    Add cascade when delete producer

--- Spring REST API/src/main/java/com/codecool/spring/model/Producer.java
+++ Spring REST API/src/main/java/com/codecool/spring/model/Producer.java
@@ -3,6 +3,7 @@ package com.codecool.spring.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,7 +15,7 @@ public class Producer {
 	@GeneratedValue
 	private long id;
 	private String name;
-	@OneToMany(mappedBy="producer")
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="producer")
 	private List<AirplaneModel> models = new ArrayList<>();
 	private String owner;
 	
