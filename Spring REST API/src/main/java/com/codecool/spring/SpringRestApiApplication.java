b810f57036eaf00de078369f67d4ab2c9c28133c
elzacodecool <elliegolec@gmail.com>
2018-10-16 12:58:39 +0200

    Delete testing method to run

--- Spring REST API/src/main/java/com/codecool/spring/SpringRestApiApplication.java
+++ Spring REST API/src/main/java/com/codecool/spring/SpringRestApiApplication.java
@@ -1,24 +1,12 @@
 package com.codecool.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.codecool.spring.service.ProducerService;
-
 @SpringBootApplication
-public class SpringRestApiApplication implements CommandLineRunner {
-	@Autowired
-	private ProducerService ps;
-	
+public class SpringRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("ok");
-		ps.createProducent("LOL", "GOL");
-	}
+
 }
