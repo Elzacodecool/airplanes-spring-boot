47ee55428a0143ef674115fcf6ca1f994f05722b
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-17 00:16:00 +0200

    Create airplaneModelService field

--- Spring REST API/src/main/java/com/codecool/spring/controller/AirplaneModelControllerSimplePath.java
+++ Spring REST API/src/main/java/com/codecool/spring/controller/AirplaneModelControllerSimplePath.java
@@ -1,8 +1,16 @@
 package com.codecool.spring.controller;
 
+import com.codecool.spring.model.AirplaneModel;
+import com.codecool.spring.service.AirplaneModelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AirplaneModelControllerSimplePath {
+
+    @Autowired
+    private AirplaneModelService airplaneModelService;
 
 }
