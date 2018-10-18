b6c187da37ed7df45734736a9bf849da2b202fbe
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-18 09:52:36 +0200

    Create custom ErrorMessage inner class

--- Spring REST API/src/main/java/com/codecool/spring/controller/GlobalExceptionHandler.java
+++ Spring REST API/src/main/java/com/codecool/spring/controller/GlobalExceptionHandler.java
@@ -23,4 +23,50 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorMessageResponse, headers, status);
     }
 }
+
+class ErrorMessage {
+
+    private String message;
+    private String path;
+    private Timestamp timestamp;
+    private String status;
+
+    public ErrorMessage(Timestamp timestamp, String status, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.message = message;
+        this.path = path;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String details) {
+        this.path = path;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
