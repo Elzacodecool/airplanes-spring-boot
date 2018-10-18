2ef6e8376682482b0d5b1bf19ee5dc62dcb98221
Elzacodecool <elliegolec@gmail.com>
2018-10-18 14:39:08 +0200

    Add logs for producer with interceptor

---
+++ Spring REST API/src/main/java/com/codecool/spring/interceptor/ProducerServiceInterceptor.java
@@ -0,0 +1,56 @@
+package com.codecool.spring.interceptor;
+
+import com.codecool.spring.service.ProducerService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+@Component
+public class ProducerServiceInterceptor implements HandlerInterceptor {
+    private static final Logger LOGGER = LogManager.getLogger(ProducerService.class.getName());
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+         HandlerMethod handlerMethod = (HandlerMethod) handler;
+         Method method = handlerMethod.getMethod();
+         LOGGER.info("[preHandle] start method: {} {} - from path: {}",
+            request.getMethod(), method.getName(), request.getRequestURI());
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+
+        LOGGER.info("[postHandle] handle the request...");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+                                Object handler, Exception exception) throws Exception {
+        if (exception != null){
+            exception.printStackTrace();
+        }
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+
+        if (response.getStatus() >= 400) {
+            LOGGER.info("[afterCompletion] Can't " + method.getName()
+                    + ", producer not found [error status: " + response.getStatus() + "]");
+        } else {
+            LOGGER.info("[afterCompletion] " + method.getName() + " is completed");
+        }
+    }
+}
