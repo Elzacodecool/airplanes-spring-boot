0f37dabe56eb41efb245d47907630c974b298d6d
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-19 13:37:52 +0200

    Refactor AirplaneModelController

--- Spring REST API/src/main/java/com/codecool/spring/controller/AirplaneModelController.java
+++ Spring REST API/src/main/java/com/codecool/spring/controller/AirplaneModelController.java
@@ -17,33 +17,28 @@ public class AirplaneModelController {
     private AirplaneModelService airplaneModelService;
 
     @GetMapping("/producers/{id}/airplanes")
-    public List<AirplaneModel> getAllAirplaneModels(@PathVariable long id) throws AirplaneModelNotFoundException,
-            JDBCConnectionException {
+    public List<AirplaneModel> getAllAirplaneModels(@PathVariable long id) {
         return airplaneModelService.getAllAirplaneModels(id);
     }
 
     @GetMapping("/producers/{producerId}/airplanes/{id}")
-    public AirplaneModel getAirplaneModel(@PathVariable long id) throws AirplaneModelNotFoundException,
-            JDBCConnectionException {
+    public AirplaneModel getAirplaneModel(@PathVariable long id) {
         return airplaneModelService.getAirplaneModel(id);
     }
 
     @PostMapping("/producers/{producerId}/airplanes")
-    public void addAirplaneModel(@RequestBody AirplaneModel airplaneModel, @PathVariable long producerId)
-            throws AirplaneModelWrongDataException, JDBCConnectionException {
+    public void addAirplaneModel(@RequestBody AirplaneModel airplaneModel, @PathVariable long producerId) {
         airplaneModelService.addAirplaneModel(producerId, airplaneModel);
     }
 
     @PutMapping("/producers/{producerId}/airplanes/{id}")
     public void updateAirplaneModel(@RequestBody AirplaneModel airplaneModel,
-                                    @PathVariable long producerId, @PathVariable long id)
-            throws AirplaneModelWrongDataException, JDBCConnectionException {
+                                    @PathVariable long producerId, @PathVariable long id) {
         airplaneModelService.updateAirplaneModel(producerId, id, airplaneModel);
     }
 
     @DeleteMapping("/producers/{producerId}/airplanes/{id}")
-    public void deleteAirplaneModel(@PathVariable long id) throws AirplaneModelWrongDataException,
-            JDBCConnectionException {
+    public void deleteAirplaneModel(@PathVariable long id) {
         airplaneModelService.deleteAirplaneModel(id);
     }
 }
